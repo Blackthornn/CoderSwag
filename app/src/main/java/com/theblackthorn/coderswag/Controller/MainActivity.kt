1f1b5a27abbd56a86f49f573d7a349e2fec13fb7
The Blackthorn <emekaujah@hotmail.co.uk>
2019-04-17 10:32:52 +1000

    prepare for recycler view adapter

--- app/src/main/java/com/theblackthorn/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/theblackthorn/coderswag/Controller/MainActivity.kt
@@ -2,12 +2,20 @@ package com.theblackthorn.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.theblackthorn.coderswag.Adapters.CategoryAdapter
 import com.theblackthorn.coderswag.R
+import com.theblackthorn.coderswag.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = CategoryAdapter(this, DataService.categories)
+        categoryListView.adapter = adapter
     }
 }
